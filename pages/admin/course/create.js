b212f1696b3c3a1a55bc9081c7978c6e1884fa0f
Mukesh Sharma <dheerajbishwaas@gmail.com>
2025-04-27 12:46:59 +0530

    Login and user dashboard admin dashborad hearder footer created

---
+++ pages/admin/course/create.js
@@ -0,0 +1,13 @@
+// pages/admin/course/create.js
+import React from 'react';
+
+const CreateCourse = () => {
+  return (
+    <div>
+      <h1>Create Course</h1>
+      {/* Your content for creating a course */}
+    </div>
+  );
+};
+
+export default CreateCourse;
---
+++ pages/admin/course/update.js
@@ -0,0 +1,13 @@
+// pages/admin/course/update.js
+import React from 'react';
+
+const UpdateCourse = () => {
+  return (
+    <div>
+      <h1>Update Course</h1>
+      {/* Your content for updating a course */}
+    </div>
+  );
+};
+
+export default UpdateCourse;
