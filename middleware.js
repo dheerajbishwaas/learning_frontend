28f25249322385ff157f6a9aa7163ea2b1c1a31f
Dheeraj bishwas <95462857+dheerajbishwaas@users.noreply.github.com>
2025-05-04 09:29:48 +0530

    Update middleware.js
    access_token
    value change

--- middleware.js
+++ middleware.js
@@ -6,7 +6,7 @@ export const config = {
 
   
 export function middleware(req) {
-  const token = req.cookies.get('token')?.value;
+  const token = req.cookies.get('access_token')?.value;
   const isAdminPath = req.nextUrl.pathname.startsWith('/admin');
   const loginUrl = new URL('/login', req.url);
 
